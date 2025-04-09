08e3ade0cdd9b9db6e452ef6d408576cdeaec00f
Kerly Sarrias <kerlysarrias011@gmail.com>
2025-04-09 17:36:24 -0500

    Ejercicio lógica de programación

---
+++ script.js
@@ -0,0 +1,21 @@
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+
+const num1 = Number(prompt("Ingrese un número  : "))
+const num2 = Number(prompt("Ingrese un número  : "))
+const num3 = Number(prompt("Ingrese un número  : "))
+
+if (num1 === num2 && num2 === num3) {
+    console.log("Los numeros son iguales")
+}
+
+const numeros = [num1, num2, num3]
+
+const menorAmayor = [...numeros].sort((a, b) => a - b)
+const mayorAmenor = [...numeros].sort((a, b) => b - a)
+
+
+
+console.log("Numeros Ingresados : ", numeros.join(","))
+console.log("Mayor a menor : ", mayorAmenor.join(","))
+console.log("Menor a Mayor : ", menorAmayor.join(","))
